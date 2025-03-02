a7d323eb8b951e849430dd29d09cae64b345f466
Maryam Valian <maryam.valian@gmail.com>
2025-03-01 22:15:35 -0500

    Error rate reached to 33.38

--- TimeSeriesPrediction.py
+++ TimeSeriesPrediction.py
@@ -107,7 +107,8 @@ def run_analysis(filename):
     # Second part: prediction---------------------------------------
     train = deseasonalized[:-20]  #all points except last 20
     test = deseasonalized[-20:]  #last 20 points
-
+    #The order is extracted from Auto-correlation figure
+    # the figure reaches to zero around 1200. to avoid overfitting Around 1000 will be good
     coeffs = estimate_ar_coefficients(train, 1000)
     print("Estimated AR coefficients:", coeffs)
     forecast_steps = len(test)
