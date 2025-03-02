a0854c30c1cae1d6adf9a635ae23c2c96e771cfc
Maryam Valian <maryam.valian@gmail.com>
2025-03-02 17:42:40 -0500

    ClR CD

--- TimeSeriesPrediction.py
+++ TimeSeriesPrediction.py
@@ -109,7 +109,7 @@ def run_analysis(filename):
     test = deseasonalized[-20:]  #last 20 points
     #The order is extracted from Auto-correlation figure
     # the figure reaches to zero around 1200.
-    # To avoid overfitting, Order will be 1000
+    # To avoid overfitting, Order will be 1000.
     coeffs = estimate_ar_coefficients(train, 1000)
     print("Estimated AR coefficients:", coeffs)
     forecast_steps = len(test)
