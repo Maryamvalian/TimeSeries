ff8b802791826e6db7bf6604476bf57c16a8c784
Maryam Valian <maryam.valian@gmail.com>
2025-03-03 16:57:36 -0500

    update

--- create_sino_audo.py
+++ create_sino_audo.py
@@ -8,7 +8,7 @@ def generate_sinusoid(frequency, duration, amplitude, samplerate):
     return signal
 
 
-#main-----------------------------------------------------
+#main--------
 
 sinusoid = generate_sinusoid(7500, 5.0, 0.5, 44000)
 sf.write("sinusoid.wav", sinusoid, 44000)
